46568ce17371bc25add203f164d1f788f0c1161b
Kevin Cristiano <kcristiano@tadpole.cc>
2015-03-11 15:36:56 -0400

    clean up base plugin file

--- tadpole-civicrm-css.php
+++ tadpole-civicrm-css.php
@@ -15,19 +15,6 @@ define( 'TC_CIVICRM_CSS_URL', plugin_dir_url( __FILE__ ) );
 define( 'TC_CIVICRM_CSS_PBASE', plugin_basename( __FILE__ ) );
 define( 'TC_CIVICRM_CSS_BASE', str_replace( basename( __FILE__ ), "", plugin_basename( __FILE__ ) ) );
 
-
-//* Set CiviCRM CSS file
-//*add_action( 'wp_print_styles', 'tc_deregister_default_civicrm_styles', 100 );*/
-function tc_deregister_default_civicrm_styles() {
-// CRM-11823 - If Civi bootstrapped, then run
-	global $civicrm_root;
-	if ( empty($civicrm_root)) {
-		return;
-	}
-	wp_deregister_style( 'civicrm/css/civicrm.css' );
-	$file = CRM_Core_Resources::singleton()->getUrl('civicrm', 'css/civicrm.css', TRUE);
-	CRM_Core_Region::instance('html-header')->update($file, array('disabled' => 'TRUE'));
-}
 add_action( 'admin_enqueue_scripts', 'tc_admin_register_tad_civicrm_styles' );
 
 function tc_admin_register_tad_civicrm_styles() {
