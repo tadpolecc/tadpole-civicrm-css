1ea53dc410bf56326d16b3f3cc92db1c6651d960
Kevin Cristiano <kcristiano@tadpole.cc>
2015-03-14 20:43:28 -0400

    Clean up absolute paths.   Introduce filters that will allow both admin and front end css files to be overridden by another plugin or theme's function.php file

--- tadpole-civicrm-css.php
+++ tadpole-civicrm-css.php
@@ -10,23 +10,26 @@ Author URI: https://tadpole.cc
 License: AGPL
 */
 
-define( 'TC_CIVICRM_CSS_DIR', dirname( __FILE__ ) );
-define( 'TC_CIVICRM_CSS_URL', plugin_dir_url( __FILE__ ) );
-define( 'TC_CIVICRM_CSS_PBASE', plugin_basename( __FILE__ ) );
-define( 'TC_CIVICRM_CSS_BASE', str_replace( basename( __FILE__ ), "", plugin_basename( __FILE__ ) ) );
 
+/*Enqueue default CiviCRM CSS in admin.  Create a filter to allow themes and other plugins to overrride */
 add_action( 'admin_enqueue_scripts', 'tc_admin_register_tad_civicrm_styles' );
 
 function tc_admin_register_tad_civicrm_styles() {
-        wp_enqueue_style ('tad_admin_civicrm',  '/wp-content/plugins/civicrm/civicrm/css/civicrm.css' );
+		$tc_civi_css_admin = (plugin_dir_url()  . 'civicrm/civicrm/css/civicrm.css');
+		$tc_civi_css_admin = apply_filters('tc_civicss_override_admin', $tc_civi_css_admin);
+        wp_enqueue_style ('tad_admin_civicrm',  $tc_civi_css_admin );
 }
 
-
+/*Enqueue custom CiviCRM CSS in front end of site.  Create a filter to allow themes and other plugins to overrride */
 add_action( 'wp_print_styles', 'tc_register_tad_civicrm_styles', 110 );
 function tc_register_tad_civicrm_styles() {
-	wp_enqueue_style ('tad_civicrm', TC_CIVICRM_CSS_URL . 'css/civicrm.css' );
+	$tc_civi_css = (plugin_dir_url( __FILE__ )  . 'css/civicrm.css') ;
+	$tc_civi_css = apply_filters ( 'tc_civicss_override' ,  $tc_civi_css ) ;
+	wp_enqueue_style ('tad_civicrm', $tc_civi_css );
 }
 
+
+/*On Activation, update CiviCRM via API to turn off Default CSS file */
 register_activation_hook( __FILE__, 'tc_civi_api');
 function tc_civi_api() {
 
@@ -35,6 +38,7 @@ civicrm_wp_initialize();
 civicrm_api3('Setting', 'create', array('disable_core_css' => 1,));
 }
 
+/*On Deactivation, update CiviCRM via API to turn on Default CSS file */
 register_deactivation_hook( __FILE__, 'tc_civi_deactivate');
 function tc_civi_deactivate() {
 
